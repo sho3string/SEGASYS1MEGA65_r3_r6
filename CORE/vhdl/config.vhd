2954383576358f851cd16f9cce98ec8ffb65c4cb
Samuel <36328867+sho3string@users.noreply.github.com>
2025-09-27 08:46:14 +1000

    Remove reference to secondbutton.vhd

--- CORE/vhdl/config.vhd
+++ CORE/vhdl/config.vhd
@@ -76,7 +76,7 @@ type WHS_RECORD_ARRAY_TYPE is array (0 to WHS_RECORDS - 1) of WHS_RECORD_TYPE;
 
 constant SCR_WELCOME : string :=
 
-   "Sega-Choplifter V0.5.0 (beta)\n"      &
+   "Sega-Choplifter V0.5.1       \n"      &
    "-----------------------------\n"      &
    "\n" &
    "MiSTer port done by Muse in 2025\n\n" &
